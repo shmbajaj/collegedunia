46f54e84e502234ef632c0dcf5ee3a530b4c8e65
Shubham Bajaj <shmbajaj32@gmail.com>
2023-11-12 09:17:10 +0000

    init main nav

--- app/components/site-header.tsx
+++ app/components/site-header.tsx
@@ -1,10 +1,12 @@
 import { MainNav } from "./main-nav";
+import { MobileNav } from "./mobile-nav";
 
 export function SiteHeader() {
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container h-14 flex items-center">
         <MainNav />
+        <MobileNav />
       </div>
     </header>
   );
