5ec5ab41fc553879382e1811b38c94205335dc90
Shubham Bajaj <shmbajaj32@gmail.com>
2023-11-12 13:15:12 +0000

    done with header, mobile-nav & main-nav

--- app/components/main-nav.tsx
+++ app/components/main-nav.tsx
@@ -3,85 +3,42 @@ import { Icons } from "./icons";
 import { cn } from "~/lib/utils";
 import { ChevronDown } from "lucide-react";
 import { Button } from "./ui/button";
+import { navConfig } from "~/config/nav";
+import { siteConfig } from "~/config/site";
 
 export function MainNav() {
   const { pathname } = useLocation();
   return (
-    <div className="mr-4 hidden md:flex">
+    <div className="m-auto max-w-5xl hidden md:flex container h-14 items-center justify-evenly">
       <Link to="/" className="mr-6 flex items-center space-x-2">
         <Icons.Logo className="h-6 w-6" />
-        <span className="hidden sm:inline-block font-bold">collegedunia</span>
+        <span className="hidden sm:inline-block font-bold">
+          {siteConfig.name}
+        </span>
       </Link>
       <nav className="flex items-center space-x-6 text-sm font-medium">
-        <Button variant="outline" className="border-none p-0 hover:bg-inherit">
+        <Button
+          variant="outline"
+          className="border-none p-0 hover:bg-inherit text-sm font-medium"
+        >
           Consultation <ChevronDown className="h-4 w-4 ml-2" />
         </Button>
-        <Link
-          to="direct-admission"
-          className={cn(
-            "transition-colors hover:text-foreground/80",
-            pathname?.startsWith("/direct-admission")
-              ? "text-foreground"
-              : "text-foreground/60"
-          )}
-        >
-          Direct Admission
-        </Link>
-        <Link
-          to="about-us"
-          className={cn(
-            "transition-colors hover:text-foreground/80",
-            pathname?.startsWith("/about-us")
-              ? "text-foreground"
-              : "text-foreground/60"
-          )}
-        >
-          About Us
-        </Link>
-        <Link
-          to="login"
-          className={cn(
-            "transition-colors hover:text-foreground/80",
-            pathname?.startsWith("/login")
-              ? "text-foreground"
-              : "text-foreground/60"
-          )}
-        >
-          Login
-        </Link>
-        <Link
-          to="registration"
-          className={cn(
-            "transition-colors hover:text-foreground/80",
-            pathname?.startsWith("/registration")
-              ? "text-foreground"
-              : "text-foreground/60"
-          )}
-        >
-          Registration
-        </Link>
-        <Link
-          to="contact-us"
-          className={cn(
-            "transition-colors hover:text-foreground/80",
-            pathname?.startsWith("/contact-us")
-              ? "text-foreground"
-              : "text-foreground/60"
-          )}
-        >
-          Contact Us
-        </Link>
-        <Link
-          to="forum"
-          className={cn(
-            "hidden transition-colors hover:text-foreground/80 lg:block",
-            pathname?.startsWith("/forum")
-              ? "text-foreground"
-              : "text-foreground/60"
-          )}
-        >
-          Forum
-        </Link>
+        <div className="flex items-center space-x-3">
+          {navConfig.mainNav.map((item) => (
+            <Link
+              key={item.to}
+              to={item.to}
+              className={cn(
+                "transition-colors hover:text-foreground/80",
+                pathname?.startsWith(`/${item.to}`)
+                  ? "text-foreground"
+                  : "text-foreground/60"
+              )}
+            >
+              {item.title}
+            </Link>
+          ))}
+        </div>
       </nav>
     </div>
   );
--- app/components/site-header.tsx
+++ app/components/site-header.tsx
@@ -4,10 +4,9 @@ import { MobileNav } from "./mobile-nav";
 export function SiteHeader() {
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container h-14 flex items-center">
-        <MainNav />
-        <MobileNav />
-      </div>
+      {/* INFO[note-for-self]: using a container is easy-way to wrap nav components and use lesser code to get more done */}
+      <MainNav />
+      <MobileNav />
     </header>
   );
 }
