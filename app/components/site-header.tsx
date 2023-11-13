ad06982fbbe7762c9967988d1eeab2d2fe4b59ba
Shubham Bajaj <shmbajaj32@gmail.com>
2023-11-13 15:52:50 +0000

    added page header section

--- app/components/site-header.tsx
+++ app/components/site-header.tsx
@@ -3,7 +3,7 @@ import { MobileNav } from "./mobile-nav";
 
 export function SiteHeader() {
   return (
-    <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+    <header className="sticky top-0 z-50 w-full border-b bg-white bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background">
       {/* INFO[note-for-self]: using a container is easy-way to wrap nav components and use lesser code to get more done */}
       <MainNav />
       <MobileNav />
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,29 +1,28 @@
 import { Link } from "@remix-run/react";
-import {
-  PageHeader,
-  PageHeaderDescription,
-  PageHeaderHeading,
-} from "~/components/page-header";
+import { PageHeader, PageHeaderHeading } from "~/components/page-header";
 import { buttonVariants } from "~/components/ui/button";
 import { cn } from "~/lib/utils";
 
 export default function Index() {
   return (
     <div className="container relative">
-      <PageHeader className="pb-8">
-        <p className="capitalize">PROFESSIONAL EDUCATION CONSULTING</p>
-        <PageHeaderHeading className="capitalize">
-          TAKE YOUR FUTURE TO NEW HEIGHTS
-        </PageHeaderHeading>
-        <div className="w-full flex items-center space-x-4 pb-8 pt-4 md:pb-10">
-          <Link to="#" className={cn(buttonVariants())}>
-            Get Started
-          </Link>
-          <Link to="#" className={cn(buttonVariants({ variant: "outline" }))}>
-            Learn More
-          </Link>
-        </div>
-      </PageHeader>
+      <section className="relative h-[580px]">
+        <PageHeader className="pb-8 absolute top-16 right-1 z-50 w-full max-w-lg ">
+          <p className="capitalize">PROFESSIONAL EDUCATION CONSULTING</p>
+          <PageHeaderHeading className="capitalize">
+            TAKE YOUR FUTURE TO NEW HEIGHTS
+          </PageHeaderHeading>
+          <div className="w-full flex items-center space-x-4 pb-8 pt-4 md:pb-10">
+            <Link to="#" className={cn(buttonVariants())}>
+              Get Started
+            </Link>
+            <Link to="#" className={cn(buttonVariants({ variant: "outline" }))}>
+              Learn More
+            </Link>
+          </div>
+        </PageHeader>
+        <div className="absolute w-full h-full top-0 left-0 bg-slate-500 bg-[url('https://catalysteducations.com/wp-content/uploads/2022/12/edit2-scaled.jpg')] bg-no-repeat bg-cover bg-center bg-fixed"></div>
+      </section>
     </div>
   );
 }
