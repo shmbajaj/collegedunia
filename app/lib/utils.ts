be2c28fee36475209e8f52348444c607cf1cc3b9
Shubham Bajaj <shmbajaj32@gmail.com>
2023-11-21 12:36:19 +0000

    validate action data

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,6 +1,34 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+import type { ZodError, ZodSchema } from "zod";
+import { ActionErrors } from "~/types/validation-action";
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export async function validationAction<ActionInput>({
+  request,
+  schema,
+}: {
+  request: Request;
+  schema: ZodSchema;
+}) {
+  const formData = await request.formData();
+  const body = Object.fromEntries(formData);
+  try {
+    const data = schema.parse(body) as ActionInput;
+    return { data, errors: null };
+  } catch (e) {
+    const _errors = e as ZodError<ActionInput>;
+    const errors = _errors.issues.reduce((acc: ActionErrors<ActionInput>, error) => {
+      const key = error.path[0] as keyof ActionInput;
+      acc[key] = error.message;
+      return acc;
+    }, {});
+    return {
+      data: body,
+      errors,
+    };
+  }
 }
