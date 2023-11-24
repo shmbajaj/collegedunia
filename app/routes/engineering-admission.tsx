a0f600df0881d5832c9851a6cf08d8f1b7e3b977
Shubham Bajaj <shmbajaj32@gmail.com>
2023-11-24 14:36:14 +0000

    added dummy section

--- app/routes/engineering-admission.tsx
+++ app/routes/engineering-admission.tsx
@@ -6,10 +6,7 @@ import {
   PageHeaderHeading,
 } from "~/components/page-header";
 import { buttonVariants } from "~/components/ui/button";
-import { Card, CardContent, CardHeader } from "~/components/ui/card";
 import { cn } from "~/lib/utils";
-import { Slide } from "pure-react-carousel";
-import { Carousel } from "~/components/carousel";
 
 export const meta: MetaFunction = () => {
   return [
@@ -59,42 +56,12 @@ export default function EngineeringAdmissions() {
           <PageHeaderHeading className="capitalize text-center md:text-left">
             Engineering Branches
           </PageHeaderHeading>
+          <PageHeaderDescription>
+            Section Under Development
+          </PageHeaderDescription>
           <hr className="mx-auto my-1 h-1 w-36 rounded border-0 bg-orange-500 md:my-4" />
         </PageHeader>
-        <div className="w-full max-w-md m-auto">
-          <Carousel
-            visibleSlides={1}
-            totalSlides={1}
-            step={1}
-            naturalSlideWidth={400}
-            naturalSlideHeight={500}
-          >
-            {Array.from({ length: 1 }).map((_, index) => (
-              <Slide index={index} key={index}>
-                <Card className="w-full border-2 rounded">
-                  {/* TODO: replace image url with own asset url */}
-                  <CardHeader className="w-full h-52 bg-blue-300 bg-[url('https://catalysteducations.com/wp-content/uploads/2022/12/13-1.jpg')] bg-repeat bg-contain bg-center"></CardHeader>
-                  <CardContent className="pt-6">
-                    <p className="uppercase text-center font-bold text-orange-500">
-                      Information Technology
-                    </p>
-                    <hr className="mx-auto my-1 h-1 bg-black rounded border-0 md:my-4" />
-                    <p className="leading-8">
-                      Information technology (IT) engineering is a field of
-                      study focused on the design, implementation, and
-                      maintenance of computer systems. It is distinct from
-                      computer engineering in that it focuses more on the use of
-                      existing systems rather than the creation and designing of
-                      new ones. IT engineering also involves working with
-                      applications, networks, databases, and other software
-                      programs.
-                    </p>
-                  </CardContent>
-                </Card>
-              </Slide>
-            ))}
-          </Carousel>
-        </div>
+        {/* TODO: use carousel */}
       </section>
     </div>
   );
