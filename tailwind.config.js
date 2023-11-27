5021bff7dbb8d94d10b21051d6b9ed7000ada952
Shubham Bajaj <shmbajaj32@gmail.com>
2023-11-27 17:47:00 +0000

    finished draft version of management page

--- tailwind.config.js
+++ tailwind.config.js
@@ -72,5 +72,23 @@ module.exports = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [
+    require("tailwindcss-animate"),
+    function ({ addUtilities }) {
+      addUtilities({
+        ".my-rotate-y-180": {
+          transform: "rotateY(180deg)",
+        },
+        ".preserve-3d": {
+          transformStyle: "preserve-3d",
+        },
+        ".perspective": {
+          perspective: "1000px",
+        },
+        ".backface-hidden": {
+          backfaceVisibility: "hidden",
+        },
+      });
+    },
+  ],
 };
